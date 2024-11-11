700295db0c83ac905f2ae2a82387a28b0f9c42cd
fanfoo <wtr.ssmt@gmail.com>
2024-11-12 02:25:23 +0900

    modify appearance of top page

--- web-page/app/page.tsx
+++ web-page/app/page.tsx
@@ -8,12 +8,12 @@ export default async function Home() {
   const pageData: PageData = createPageData(1, posts.length);
 
   return (
-    <div className="container w-full h-full flex justify-center">
-      <div className="prose dark:prose-invert md:max-w-3xl w-2/3 m-2">
+    <div className="w-full flex justify-center">
+      <div className="container prose dark:prose-invert md:max-w-3xl m-2 flex-1">
         <div>
-          <h1>Artists&apos; wisdom</h1>
+          <h1>Painters&apos; wisdom</h1>
         </div>
-        <div className="w-full grid grid-cols-2 gap-2">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-2">
           {posts.slice(pageData.start, pageData.end).map((post) => (
             <PostCard key={post.slug.join('/')} post={post} />
           ))}
--- web-page/components/PostCard.tsx
+++ web-page/components/PostCard.tsx
@@ -5,9 +5,9 @@ const PostCard = ({ post }: { post: PostItem }) => {
   return (
     <Link
       href={`/posts/${post.slug.join('/')}`}
-      className="align-self-baseline col-lg-4 d-flex flex-column justify-content-between scale-95 hover:scale-100 ease-in duration-100"
+      className="align-baseline flex flex-col justify-between scale-95 hover:scale-100 ease-in duration-100"
     >
-      <div className="bg-gray-200 dark:bg-gray-800">
+      <div className="bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700">
         <div className="px-2 py-3 mt-auto">
           <h1 className="font-bold text-lg">{post.title}</h1>
           <span className="badge bg-secondary text-white">{post.date}</span>
